b8183ed7c952158cd36004cc76448ccca787172b
iq-acue <iquigley@acue.org>
2026-05-04 14:53:49 -0400

    updating email

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
 // Lodge contact details
 define('LODGE_RECIPIENT_EMAIL', 'guilford656@gmail.com');
 define('LODGE_RECIPIENT_NAME',  'Guilford Lodge #656');
-define('CC_EMAIL',              'cc@example.com');
+define('CC_EMAIL',              'ikequigley@gmail.com');
 define('FROM_EMAIL',            'ikequigley@gmail.com');
 define('FROM_NAME',             'Guilford Lodge #656 Website');
 define('CONTACT_PHONE',         '(919) 244-7275');
